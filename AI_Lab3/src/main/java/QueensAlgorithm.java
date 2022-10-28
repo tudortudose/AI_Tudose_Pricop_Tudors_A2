687f816c7ea96eabf353f9dcc9c3a29ceccf7be4
pricoptudor <41987455+pricoptudor@users.noreply.github.com>
2022-10-28 10:09:19 +0300

    Update QueensAlgorithm.java

--- AI_Lab3/src/main/java/QueensAlgorithm.java
+++ AI_Lab3/src/main/java/QueensAlgorithm.java
@@ -20,34 +20,6 @@ public class QueensAlgorithm {
 
     private Set<String> queensSolutions = new HashSet<>();
 
-    //    public void findQueensBKT(int[] queensPositions, int currentQueenIndex,
-//                              List<List<Integer>> queensDomains) {
-//        if (currentQueenIndex == queensNumber) {
-//            System.out.println("I have found a solution: ");
-//            System.out.println(Arrays.toString(queensPositions));
-//            return;
-//        }
-//
-//        List<Integer> queenDomainMRV=
-//                    getQueenDomainMRV(queensDomains.get(queensPositions[currentQueenIndex]), queensDomains);
-//
-//        for (Integer nextQueenPosition : queenDomainMRV) {
-//            int[] newQueensPositions = Arrays.copyOf(queensPositions, queensNumber);
-//            List<List<Integer>> newQueensDomains =
-//                    forwardChecking(currentQueenIndex, queensDomains, nextQueenPosition, newQueensPositions);
-//            System.out.println("///\n" + Arrays.toString(queensPositions));
-//            System.out.println(queenDomainMRV);
-//            System.out.println("ITERARE:");
-//            System.out.println(nextQueenPosition);
-//            System.out.println(newQueensDomains);
-//            System.out.println(Arrays.toString(newQueensPositions));
-//            if (newQueensDomains != null && queensDomains.get(currentQueenIndex).contains(nextQueenPosition)) {
-//                newQueensPositions[currentQueenIndex] = nextQueenPosition;
-//
-//                findQueensBKT(newQueensPositions, currentQueenIndex + 1, newQueensDomains);
-//            }
-//        }
-//    }
     private boolean isSolution(int[] queensPositions) {
         for (int i = 0; i < queensNumber; ++i) {
             if (queensPositions[i] == -1) {
@@ -64,16 +36,10 @@ public class QueensAlgorithm {
             newQueensPositions[currentQueenIndex] = queenPosition;
 
             if (isSolution(newQueensPositions)) {
-//                System.out.println("Found a solution: ");
-//                System.out.println(Arrays.toString(newQueensPositions));
                 queensSolutions.add(Arrays.toString(newQueensPositions));
             } else {
-//                System.out.println("DOMAINS");
-//                System.out.println(currentQueenDomains);
                 List<List<Integer>> newQueensDomains =
                         forwardChecking(currentQueenIndex, currentQueenDomains, currentQueenIndex, newQueensPositions);
-//                System.out.println("DOMAINS AFTER");
-//                System.out.println(currentQueenDomains);
 
                 if (newQueensDomains != null) {
                     List<Integer> notVisited = new ArrayList<>();
@@ -89,53 +55,17 @@ public class QueensAlgorithm {
                     List<Integer> queenDomainMRV =
                             getQueenDomainMRV(notVisited, newQueensDomains);
 
-//                    System.out.println("///\n" + Arrays.toString(queensPositions));
-//                    System.out.println(Arrays.toString(newQueensPositions));
-//                    System.out.println(notVisited);
-//                    System.out.println(queenDomainMRV);
-//                    System.out.println("ITERARE:");
-//                    System.out.println(currentQueenIndex);
-//                    System.out.println(newQueensDomains);
-
                     for (Integer nextQueenPosition : queenDomainMRV) {
                         findQueensBKT(newQueensPositions, nextQueenPosition, newQueensDomains);
                     }
                 }
-
-//                    List<Integer> notVisited = new ArrayList<>();
-//                for(int i=0;i<queensNumber;++i){
-//                    notVisited.add(i);
-//                }
-//                for(int i=0;i<queensNumber;++i){
-//                    notVisited.remove(Integer.valueOf(newQueensPositions[i]));
-//                }
-//
-//                List<Integer> queenDomainMRV=
-//                    getQueenDomainMRV(notVisited, queensDomains);
-//
-//                for (Integer nextQueenPosition : queenDomainMRV) {
-//                    List<List<Integer>> newQueensDomains =
-//                            forwardChecking(currentQueenIndex, queensDomains, nextQueenPosition, newQueensPositions);
-//                    System.out.println("///\n" + Arrays.toString(queensPositions));
-//                    System.out.println(queenDomainMRV);
-//                    System.out.println("ITERARE:");
-//                    System.out.println(nextQueenPosition);
-//                    System.out.println(newQueensDomains);
-//                    System.out.println(Arrays.toString(newQueensPositions));
-//                    if (newQueensDomains != null && queensDomains.get(currentQueenIndex).contains(nextQueenPosition)) {
-//                        findQueensBKT(newQueensPositions, nextQueenPosition, newQueensDomains);
-//                    }
-//                }
             }
         }
     }
 
     private List<Integer> getQueenDomainMRV(List<Integer> queenDomain, List<List<Integer>> queensDomains) {
-//        System.out.println("SORTING:");
         List<Integer> newQueenDomain = new ArrayList<>(queenDomain);
-//        System.out.println(newQueenDomain);
         newQueenDomain.sort(Comparator.comparingInt(pos -> queensDomains.get(pos).size()));
-//        System.out.println(newQueenDomain);
         return newQueenDomain;
     }
 
@@ -160,9 +90,6 @@ public class QueensAlgorithm {
                 return null;
             }
         }
-
-//        System.out.println("HERE");
-//        System.out.println(newQueensDomains);
 
         for (int i = currentQueenIndex + 1, j = 1; i < queensNumber; ++i, ++j) {
             removeCollidingQueenPositions(newQueensDomains.get(i), queensPositions[currentQueenPosition], j);
@@ -242,20 +169,15 @@ public class QueensAlgorithm {
         queensAlgorithm.initQueensDomains();
         queensAlgorithm.printMatrix();
         queensAlgorithm.printDomains();
+        
         List<Integer> allPositions = new ArrayList<>();
         for (int i = 0; i < queensAlgorithm.getQueensNumber(); ++i) {
             allPositions.add(i);
         }
-//        System.out.println("HERE " + queensAlgorithm.getQueenDomainMRV(allPositions, queensAlgorithm.getQueensDomains()));
         for (Integer startPosition : queensAlgorithm.getQueenDomainMRV(allPositions, queensAlgorithm.getQueensDomains())) {
-//            System.out.println("FFS " + startPosition);
-//            System.out.println("HEREHERE " + queensAlgorithm.getQueensDomains());
             queensAlgorithm.findQueensBKT(
                     queensAlgorithm.getQueensPositions(), startPosition, queensAlgorithm.getQueensDomains());
         }
-//        queensAlgorithm.findQueensBKT(
-//                queensAlgorithm.getQueensPositions(), 0, queensAlgorithm.getQueensDomains());
-
         queensAlgorithm.printSolution();
     }
 }
